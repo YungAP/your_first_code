0034d9318adc819bc9692f3706f5476ef3e37b46
YungAP <72463850+YungAP@users.noreply.github.com>
2020-10-24 11:53:05 -0500

    Update main.c
    My first code

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
-}
+
+
+int main() {
+   int i;
+   for (i = 0; i < 32; i++){
+       printf("holberton\n");
+       printf("Hello School\n");
+   } 
+    return 0;
+}  
+  
